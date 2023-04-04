3770ff5cf40fdee08844a220da6ec9ea48ba5761
Lucas Pereira <123104884+Lucas-Pereira26@users.noreply.github.com>
2023-04-04 08:59:49 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+var modoTema = document.getElementById('modo-tema')
+var root = document.documentElement
+
+modoTema.addEventListener ('click', ()=>{
+    root.classList.toggle('tema-ofcourse')
+}
+
+);
